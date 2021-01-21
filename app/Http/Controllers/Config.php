95ea06f9bd8733da9389cb8ea8819a8cb41ca351
douglasigt14 <55840011+douglasigt14@users.noreply.github.com>
2021-01-21 10:29:52 -0300

    Editar descrição evoluindo

--- app/Http/Controllers/Config.php
+++ app/Http/Controllers/Config.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use DB;
 
 class Config extends Controller
 {
     public function index($documento_id = null) {
+        $documento = DB::select("SELECT * FROM documentos WHERE id = $documento_id");
+        $doc_descricao = $documento[0]->descricao ?? NULL;
 
-         return view('config', compact(["documento_id"]));
+        return view('config', compact(["documento_id","documento","doc_descricao"]));
     }
 }
--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -4,9 +4,21 @@
 @section('conteudo')
 <div class="panel">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Configurações Documento: X</h3>
+                    <h3>Configurações</h3>
+                    <h4>Documento: {{$doc_descricao}}</h4>
                 </div>
                 <div class="panel-body">
+                    <div class="row">
+                        <div class="col col-md-10">
+                            <label>Descrição</label>
+                            <input type="text" name='descricao' value="{{$doc_descricao}}" class="form-control" required>
+                        </div>
+                        <div class="col col-md-2">
+                            <label>&nbsp;</label>
+                            
+                            <button class="btn btn-warning btn-block" @if(!$doc_descricao) disabled @endif>Editar</button>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
