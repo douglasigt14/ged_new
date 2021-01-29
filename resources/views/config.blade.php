819ab7a401437b4b0d5ca60a5f619b05060d91eb
Douglas Rocha <douglas_igt14@hotmail.com>
2021-01-29 09:02:20 -0300

    Alterações na view Config

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -21,11 +21,11 @@
                                 @method('put')
                                 <input type="hidden" name="id" value={{$documento_id}}>
                                 <div class="col col-md-7">
-                                    <label>Editar descrição</label>
+                                    <label>Descrição</label>
                                     <input type="text" autocomplete="off" name='descricao' value="{{$doc_descricao}}" class="form-control" required>
                                 </div>
                                 <div class="col col-md-3">
-                                    <label>Data Vencimento</label>
+                                    <label>Data de Vencimento</label>
                                     <input type="date" autocomplete="off" name='dt_vencimento' value="{{$dt_vencimento}}" class="form-control" >
                                 </div>
                                 <div class="col col-md-2">
