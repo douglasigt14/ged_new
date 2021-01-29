cab2d12d50ab24c6270ace2e9b44e69ef89df0f2
Douglas Rocha <douglas_igt14@hotmail.com>
2021-01-29 09:22:56 -0300

    Alterações na view Documento

--- resources/views/documentos.blade.php
+++ resources/views/documentos.blade.php
@@ -79,8 +79,10 @@
                                 <thead>
                                     <tr>
                                         <th>Descrição</th>
+                                        @if ($setor != 'DIRETORIA')
                                         <th>Set.Anterior</th>
                                         <th>Set.Atual</th>
+                                        @endif
                                         <th class='center'>Status</th>
                                         <th class='center'>Arquivo</th>
                                         <th class='center'>Info</th>
@@ -117,10 +119,12 @@
                                             @endif
                                             
                                         </td>
+                                        @if ($setor != 'DIRETORIA')
                                         <td>{{ $item->setor_anterior }}</td>
                                         <td @if ($item->setor_atual == $setor)
                                                 class='negrito'   
                                             @endif>{{ $item->setor_atual }}</td>
+                                        @endif
                                         <td><center><p 
                                                         @if ((($item->setor_atual == $setor or ($item->tipo_passo == 'BPMN:EXCLUSIVEGATEWAY' or $item->tipo_passo == 'EXCLUSIVEGATEWAY')) and $item->finalizado != 1 ) and $setor != 'DIRETORIA')
                                                             data-toggle="modal" 
