10e95b984f97e95a029ea0b3e4f4e0c086b9b068
Douglas Rocha <douglas_igt14@hotmail.com>
2021-02-05 13:57:00 -0300

    Isolei o arquivo funcionarios.js

--- resources/views/funcionarios.blade.php
+++ resources/views/funcionarios.blade.php
@@ -197,52 +197,9 @@
     </div>
   </div>
 </div>
-@push('scripts')
-    <script>
-        $(document).ready( function () {
-                    $('.myTable').DataTable();
-        } );
-         function mostrarModal(event) {
-                const button = event.currentTarget
-                const rotulo = document.querySelector("#modalEditar #item-rotulo")
-                const nome = document.querySelector("#modalEditar #item-nome")
-                const id = document.querySelector("#modalEditar #item-id")
-                const setor_id = document.querySelector("#modalEditar #item-setor_id")
 
-                rotulo.value = button.getAttribute("data-item-rotulo")
-                nome.value = button.getAttribute("data-item-nome")
-                id.value = button.getAttribute("data-item-id")
-
-                
-
-                opcoes = [...setor_id.options]
-                opcoes.forEach(function (opcao) {
-                    if (opcao.innerText.trim() == button.getAttribute("data-item-setor").trim()) {
-                        setor_id.selectedIndex = opcao.index 
-                    }else{
-                        console.log('false')
-                    }
-                })
-            }
-
-
-            function mostrarModalSenha(event) {
-                const button = event.currentTarget
-                const id = document.querySelector("#modalSenha #item-senha-id")
-                id.value = button.getAttribute("data-item-senha-id")
-            }
-
-             function onAddOperator(e) {
-                  e.preventDefault()
-
-                  const form = e.target;
-                  if(form.senha.value !== form.confirmar_senha.value) {
-                      alert("As senhas devem ser iguais")
-                  } else {
-                      form.submit();
-                  }
-
-              }
-    </script>
-@endpush
 @endsection
+
+@push('scripts')
+        <script src="{{url('js/funcionarios.js')}}?{{date("YmdHis")}}"> </script>
+@endpush 
