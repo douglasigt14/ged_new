9002ea5dbecaa1f38b57a1d265be63380d868406
Douglas Rocha <douglas_igt14@hotmail.com>
2021-02-22 13:33:00 -0300

    Modificações de aparencia

--- public/js/documentos.js
+++ public/js/documentos.js
@@ -2,7 +2,7 @@
 $(document).ready( function () {
     var tabelas = $('.myTable').DataTable({
         "pageLength": 100,
-        "order": [[ 3, "asc" ]]
+        "order": [[ 1, "asc" ]]
     });
     var pesquisa = localStorage.getItem('pesquisa');
     if(pesquisa){
