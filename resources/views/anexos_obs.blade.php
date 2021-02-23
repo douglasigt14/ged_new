186db84218eb569c1dcf5b9843007ee067ccdf10
Douglas Rocha <douglas_igt14@hotmail.com>
2021-02-23 10:26:05 -0300

    Modal Arquivo Anexos

--- resources/views/anexos_obs.blade.php
+++ resources/views/anexos_obs.blade.php
@@ -92,7 +92,21 @@
 											<tr>
 												<td>{{$item->descricao}}</td>
 												<td class="center">{{$item->usuario}} <br>{{$item->systemdate }}</td>
-												<td><center><a target='_blank' href='{{$item->caminho}}' class="btn btn-sm btn-success"><i class="fa fa-file"></i></a></center> </td>
+												<td><center>
+												@if ($setor != 'DIRETORIA')
+													<button
+                                                    type='button' 
+                                                    data-toggle="modal" 
+                                                data-target="#modalArquivo" 
+                                                onclick="mostrarModalArquivo(event)"
+                                                data-item-id="{{$item->id}}"
+                                                data-item-descricao="{{$item->descricao}}"
+                                                data-item-caminho="{{$item->caminho}}" class="btn btn-success btn-sm cinza-ardosia"><i class="fa fa-file"></i></button>
+												@else
+													<a target='_blank' href='{{$item->caminho}}' class="btn btn-sm btn-success cinza-ardosia"><i class="fa fa-file"></i></a>
+												@endif
+												
+												</center> </td>
 												@if ($setor != 'DIRETORIA')
 												 <td>
 													@if ($item->usuario_id == $_SESSION['id'])
@@ -226,6 +240,34 @@
   </div>
 </div>
 
+
+<div class="modal fade" id="modalArquivo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-pdf" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <div class="row">
+            <div class="col col-md-1">
+                <center><button class='btn btn-default' data-dismiss="modal"><i class="fa fa-arrow-left"></i></button></center>
+            </div>
+            <div class="col col-md-9">
+                <h4><span class='negrito' id='item-descricao'></span></h4>
+            </div>
+            <div class="col col-md-2">
+                
+            </div>
+        </div>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+               <div class="col col-md-12">
+                      <iframe class='iframe-pdf' src="" id='item-iframe' frameborder="0"></iframe>
+               </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 	@push('scripts')
 		<script>
 			function mostrarModal(event) {
@@ -236,6 +278,16 @@
                 descricao.innerHTML = button.getAttribute("data-item-descricao")
                 id.value = button.getAttribute("data-item-id")
             }
+
+
+			function mostrarModalArquivo(event) {     
+					const button = event.currentTarget
+					const iframe = document.querySelector("#modalArquivo #item-iframe")
+					const descricao = document.querySelector("#modalArquivo #item-descricao")
+
+					iframe.src = button.getAttribute("data-item-caminho")
+					descricao.innerHTML = button.getAttribute("data-item-descricao")
+				}
 			   $(document).ready( function () {
                     $('.myTable').DataTable();
                 } );
